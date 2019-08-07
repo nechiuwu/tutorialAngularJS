33e5c59feb41115be0cf31a0a1947be921ab1eb7
Neysa876 <53878673+Neysa876@users.noreply.github.com>
2019-08-07 18:12:54 -0400

    Update app.js

--- Tutorial Angular JS/ips/js/app.js
+++ Tutorial Angular JS/ips/js/app.js
@@ -7,7 +7,9 @@ var app = angular.module('ejemplosApp',[ ]);
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
   //http://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK
-	
+	$http.jsonp('http://www.geoplugin.net/json.gp?jsoncallback=JSON_CALLBACK').success(function(data){
+  	$scope.geo=data;
+	});	
 
 
 
